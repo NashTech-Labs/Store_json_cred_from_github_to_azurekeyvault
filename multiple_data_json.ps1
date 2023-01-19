da9d339e2754f8a623ce740b56defd0a6e9b4663
Balraj0017 <95099000+Balraj0017@users.noreply.github.com>
2023-01-19 19:43:11 +0530

    Update multiple_data_json.ps1

--- multiple_data_json.ps1
+++ multiple_data_json.ps1
@@ -1,4 +1,4 @@
-$response = Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Balraj0017/ADO_cred/main/test2.json' -UseBasicParsing
+$response = Invoke-WebRequest -Uri 'https://xyz/test2.json' -UseBasicParsing
 $response = $response | ConvertFrom-Json 
 foreach ($r in $response.GetEnumerator()) {
     $SecureString = ConvertTo-SecureString $r.SecretKey -AsPlainText -Force
