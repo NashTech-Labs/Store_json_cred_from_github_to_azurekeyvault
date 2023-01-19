123bc449bf90ec973571775bbc49ffaa53f05d00
Balraj0017 <95099000+Balraj0017@users.noreply.github.com>
2023-01-19 19:41:33 +0530

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,25 @@
+[{
+		"ApplicationName": "753456.Abcv.WFcvsdfa",
+		"SecretKey": "TTDRkNDJjOTAtZjBhYi00NctYjkwYTNlMjNmZTQy"
+	},
+	{
+		"ApplicationName": "9045647.GQERDF.GDEAGJWSasdf",
+		"SecretKey": "OTlmMmMxYjgtZjEzMS00MTkwLWI1NDQtYjI2MT"
+	},
+	{
+		"ApplicationName": "1523456.Abcv.WFcvsdfa",
+		"SecretKey": "NDRkNDJjOTAtZjBhYi00NctYjkwYTNlMjNmZTQy"
+	},
+	{
+		"ApplicationName": "3645647.GQERDF.GDEAGJWSasdf",
+		"SecretKey": "OTlmMmMxYjgtZjEzMS00MTkwLWI1NDQtYjI2MT"
+	},
+	{
+		"ApplicationName": "5423456.Abcv.WFcvsdfa",
+		"SecretKey": "NDRkNDJjOTAtZjBhYi00NctYjkwYTNlMjNmZTQy"
+	},
+	{
+		"ApplicationName": "6445647.GQERDF.GDEAGJWSasdf",
+		"SecretKey": "OTlmMmMxYjgtZjEzMS00MTkwLWI1NDQtYjI2MT"
+	}
+]
